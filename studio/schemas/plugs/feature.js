a1faaf76295e1dcc63ed32563cc19758940cb72f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-05 11:00:57 +0800

    Add feature plugin

---
+++ studio/schemas/plugs/feature.js
@@ -0,0 +1,18 @@
+export default {
+  type: 'object',
+  name: 'feature',
+  fields: [
+    {
+      type: 'string',
+      name: 'title'
+    },
+    {
+      type: 'image',
+      name: 'icon'
+    },
+    {
+      type: 'simpleBlockContent',
+      name: 'text'
+    },
+  ]
+}
