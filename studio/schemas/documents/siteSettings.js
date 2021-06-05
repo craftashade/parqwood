17a76e0449ebbdce0f01bd3f2e791bd3940cae27
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-05 10:59:53 +0800

    Add address to site settings

--- studio/schemas/documents/siteSettings.js
+++ studio/schemas/documents/siteSettings.js
@@ -16,16 +16,24 @@ export default {
       type: 'openGraph'
     },
     {
-      type: 'color',
-      name: 'primaryColor',
-      title: 'Primary brand color',
-      description: 'Used to generate the primary accent color for websites, press materials, etc'
+      name: 'address',
+      type: 'simpleBlockContent',
+      title: 'Address'
     },
     {
-      type: 'color',
-      name: 'secondaryColor',
-      title: 'Secondary brand color',
-      description: 'Used to generate the secondary accent color for websites, press materials, etc'
-    }
+      name: 'mobile',
+      type: 'string',
+      title: 'Mobile'
+    },
+    {
+      name: 'tel',
+      type: 'string',
+      title: 'Tel'
+    },
+    {
+      name: 'email',
+      type: 'string',
+      title: 'Email'
+    },
   ]
 }
