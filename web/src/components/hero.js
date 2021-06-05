1466e183e4f64f9641c3226ae34dd8f94f62c618
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-05 14:45:23 +0800

    Update hero component

--- web/src/components/hero.js
+++ web/src/components/hero.js
@@ -2,6 +2,8 @@ import React from "react";
 import PortableText from "./portableText";
 import clientConfig from "../../client-config";
 import CTALink from "./CTALink";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 import { getGatsbyImageData } from "gatsby-source-sanity";
 import { GatsbyImage } from "gatsby-plugin-image";
@@ -16,7 +18,7 @@ const maybeImage = illustration => {
 
     img = (
       <GatsbyImage
-        className="w-full md:w-4/5 z-50"
+        className="w-full z-50"
         image={imageData}
         alt={illustration.image.alt}
       />
@@ -26,26 +28,56 @@ const maybeImage = illustration => {
 };
 
 function Hero(props) {
-  const img = maybeImage(props.illustration);
+  const { slides, cta, tagline, heading } = props
   return (
-    <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-      {/* Left col */}
-      <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-        <p className="uppercase tracking-loose w-full">{props.label}</p>
-        <h1 className="my-4 text-5xl font-bold leading-tight">{props.heading}</h1>
-        <div className="leading-normal text-2xl mb-8">
-          <PortableText blocks={props.tagline} />
-        </div>
-        {props.cta && props.cta.title && (
-          <CTALink
-            {...props.cta}
-            buttonActionClass="mx-auto ml-4 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
+    <section class="relative">
+      <Carousel showStatus={false} showThumbs={false} className="dots-left" renderIndicator={(onClickHandler, isSelected, index, label) => {
+        const indicatorClasses = "inline-block h-2 mr-2 w-10 rounded mt-8 mb-6 bg-white"
+        if (isSelected) {
+          return (
+            <li
+              className={indicatorClasses}
+              aria-label={`Selected: ${label} ${index + 1}`}
+              title={`Selected: ${label} ${index + 1}`}
+            />
+          );
+        }
+        return (
+          <li
+            className={`${indicatorClasses} opacity-50`}
+            onClick={onClickHandler}
+            onKeyDown={onClickHandler}
+            value={index}
+            key={index}
+            role="button"
+            tabIndex={0}
+            title={`${label} ${index + 1}`}
+            aria-label={`${label} ${index + 1}`}
           />
-        )}
+        );
+      }}>
+        {slides.length && slides.map(slide => {
+          console.log(slide)
+          const img = maybeImage(slide);
+          return (
+            <div>
+              {img}
+            </div>
+          )
+        })}
+      </Carousel>
+      <div class="mx-auto flex p-24 md:flex-row flex-col items-center absolute text-white" style={{ top: "20%" }}>
+        <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 class="title-font sm:text-5xl text-3xl leading-none mb-4 font-bold">{heading}</h1>
+          <p class="mb-8 leading-relaxed text-lg">
+            <PortableText blocks={tagline} />
+          </p>
+          <CTALink {...cta} buttonActionClass="bg-airbnb border border-airbnb py-3 px-10 focus:outline-none hover:bg-transparent rounded-xl text-sm hover:text-airbnb"></CTALink>
+        </div>
+        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 hidden lg:block">
+        </div>
       </div>
-      {/* Right col */}
-      <div className="w-full md:w-3/5 py-6 text-center">{img}</div>
-    </div>
+    </section>
   );
 }
 
