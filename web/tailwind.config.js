8f01cdb7d63c6f8ef2666569327a0ee215640958
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-05 10:58:59 +0800

    Add tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
+  mode: 'jit',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        cas: "#20215B",
+        sunshine: "#F6C35C",
+        grey: "#CFD9E1",
+        airbnb: "#FA565D"
+      }
+    },
+    fontFamily: {
+      'body': ['Source Sans Pro'],
+    },
   },
   variants: {},
   plugins: []
