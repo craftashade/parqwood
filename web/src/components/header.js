de80cc58c9ffbfcf48e46d37768e2ea3be042c3e
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-05 14:45:10 +0800

    Update header component

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,67 +1,66 @@
-import { Link } from "gatsby";
 import React from "react";
 import CTALink from "./CTALink";
+import Logo from "../images/cas-logo.svg"
+import { getHref } from "../lib/helpers"
 
-const Header = ({ showNav, siteTitle, scrolled, navMenuItems = [], textWhite = true }) => {
+const Header = ({ showNav, scrolled, navMenuItems = [], data }) => {
   let headerClass = "fixed w-full z-30 top-0 text-white";
   headerClass += scrolled ? " bg-white shadow" : "";
 
-  let navActionClass =
-    "mx-auto lg:mx-0 hover:underline font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75";
-  navActionClass += !textWhite || !scrolled ? " bg-white text-gray-800" : "";
-  navActionClass += textWhite || scrolled ? " gradient text-white" : "";
+  let navActionClass = "mx-auto lg:mx-0 rounded-xl mt-4 lg:mt-0 py-4 px-6 border-2 hover:bg-white hover:text-black";
 
   let navContentClass =
-    "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20";
-  navContentClass += !textWhite || !scrolled ? " lg:bg-transparent bg-gray-100" : "";
-  navContentClass += textWhite || scrolled ? " bg-white" : "";
+    "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 p-4 lg:p-0 z-20";
 
-  let titleClass = "toggleColour no-underline hover:no-underline font-bold text-2xl lg:text-4xl";
-  titleClass += !textWhite || scrolled ? " text-gray-800" : "";
-  titleClass += textWhite || !scrolled ? " text-white" : "";
+  const { navs } = data
+  let socials = navs.edges.find(n => n.node.title === "Social URLs")
+  if (socials) socials = socials.node.items
+
+  let headerCTA = navs.edges.find(n => n.node.title === "Header CTA")
+  if (headerCTA) headerCTA = headerCTA.node.items
 
   return (
-    <nav id="header" className={headerClass}>
-      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
-        <div className="pl-4 flex items-center">
-          <Link id="siteTitle" className={titleClass} to="/">
-            <svg
-              className="h-8 fill-current inline"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512.005 512.005"
-            >
-              <rect
-                fill="#2a2a31"
-                x="16.539"
-                y="425.626"
-                width="479.767"
-                height="50.502"
-                transform="matrix(1,0,0,1,0,0)"
-              />
-              <path
-                className="plane-take-off"
-                d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
-              />
-            </svg>{" "}
-            {siteTitle}
-          </Link>
-        </div>
-
-        {showNav && navMenuItems && (
-          <div className={navContentClass} id="nav-content">
-            <ul className="list-reset lg:flex justify-end flex-1 items-center">
-              {navMenuItems.map(i => (
-                <li className="mr-3">
-                  <CTALink {...i} buttonActionClass={navActionClass} />
+    <header class="text-white font-body absolute w-full z-10">
+      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+          <a class="mr-5">Services</a>
+          <a class="mr-5">Brands</a>
+          <a class="mr-5">Projects</a>
+        </nav>
+        <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center lg:items-center lg:justify-center mb-4 md:mb-0">
+          <Logo />
+        </a>
+        <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+          {showNav && navMenuItems && (
+            <div className={navContentClass} id="nav-content">
+              <ul className="list-reset lg:flex justify-end flex-1 items-center">
+                {navMenuItems.map(i => (
+                  <li className="mr-6">
+                    <CTALink {...i} buttonActionClass={navActionClass} />
+                  </li>
+                ))}
+                <li className="mr-6">
+                  <a className="" href={getHref(socials.find(s => s.title === "Instagram"))} target="_blank">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                      <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                  </a>
                 </li>
-              ))}
-            </ul>
-          </div>
-        )}
+                <li className="mr-6">
+                  <a className="" href={getHref(socials.find(s => s.title === "Facebook"))} target="_blank">
+                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                  </a>
+                </li>
+                {headerCTA && <CTALink {...headerCTA[0]} buttonActionClass="border-2 border-white rounded-2xl py-3 px-8 hover:bg-white hover:text-cas" />}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
-
-      <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
-    </nav>
+    </header>
   );
 };
 
