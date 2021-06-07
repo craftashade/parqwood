e5c2a8a31effe5c7863956d665989845a9f2f52c
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-07 10:52:25 +0800

    Add service document schema

---
+++ studio/schemas/documents/service.js
@@ -0,0 +1,41 @@
+export default {
+  type: 'document',
+  name: 'service',
+  title: 'Service',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'serviceCategory',
+      type: 'reference',
+      // weak: true, // Uncomment if you want to be able to delete navigation even though pages refer to it
+      to: [{ type: 'serviceCategory' }],
+      title: 'Service Category',
+      description: 'Category of service',
+    },
+    {
+      name: 'excerpt',
+      type: 'text',
+      title: 'Excerpt'
+    },
+    {
+      name: 'text',
+      type: 'simpleBlockContent',
+      title: 'Text'
+    },
+    {
+      name: 'thumbnail',
+      type: 'illustration',
+      title: 'Featured Image'
+    },
+    {
+      name: 'images',
+      type: 'array',
+      title: 'Images',
+      of: [{ type: 'illustration' }]
+    }
+  ],
+}
