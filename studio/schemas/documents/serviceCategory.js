13cdf5d2f7fcf0b333cbdadc3f1caffdd741cabf
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-07 10:52:35 +0800

    Add service category document schema

---
+++ studio/schemas/documents/serviceCategory.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'serviceCategory',
+  type: 'document',
+  title: 'Service Category',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description'
+    },
+    {
+      name: 'image',
+      type: 'illustration',
+      title: 'Image'
+    }
+  ]
+}
