5e1d594e68c09cc675b0fa615151fc0ecedeb75d
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-07 11:09:35 +0800

    Add service template for service page

---
+++ web/src/templates/service.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Container from "../components/container";
+import GraphQLErrorList from "../components/graphql-error-list";
+import Service from "../components/service";
+import SEO from "../components/seo";
+import Layout from "../containers/layout";
+import { toPlainText } from "../lib/helpers";
+
+export const query = graphql`
+  query ServiceTemplateQuery($id: String!) {
+    frontpage: sanityPage(title: {eq: "Frontpage"}) {
+      ...PageInfo
+    }
+    service: sanityService(id: { eq: $id }) {
+      id
+      images {
+        image {
+          ...SanityImage
+        }
+      }
+      title
+      _rawText
+      serviceCategory {
+        title
+      }
+    }
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+      openGraph {
+        title
+        description
+        image {
+          ...SanityImage
+        }
+      }
+      _rawAddress
+      mobile
+      tel
+      email
+    }
+    navs: allSanityNavigationMenu {
+      edges {
+        node {
+          title
+          items {
+            title
+            route
+            link
+            kind
+            _rawLandingPageRoute
+          }
+        }
+      }
+    }
+    services: allSanityService {
+      nodes {
+        title
+        serviceCategory {
+          title
+        }
+      }
+    }
+    categories: allSanityServiceCategory {
+      nodes {
+        title
+        image {
+          image {
+            ...SanityImage
+          }
+        }
+      }
+    }
+  }
+`;
+
+const ServiceTemplate = props => {
+  const { data, errors } = props;
+  const service = data && data.service;
+  return (
+    <Layout showNav={true} data={data} textWhite={false}>
+      {errors && <SEO title="GraphQL Error" />}
+      {service && (
+        <SEO
+          title={service.title || "Untitled"}
+          description={toPlainText(service._rawExcerpt)}
+          image={service.mainImage}
+        />
+      )}
+
+      {errors && (
+        <Container>
+          <GraphQLErrorList errors={errors} />
+        </Container>
+      )}
+
+      {service && <Service {...service} data={data} />}
+    </Layout>
+  );
+};
+
+export default ServiceTemplate;
