3bbe558e4280b79a9b7ff92744cbe30a2ca2d417
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-10 17:05:31 +0800

    Update cta component

--- web/src/components/cta.js
+++ web/src/components/cta.js
@@ -2,18 +2,18 @@ import React from "react";
 import PortableText from "../components/portableText";
 import CTALink from "./CTALink";
 
-const CTA = ({ label, title, body, cta }) => (
-  <section className="bg-sunshine container mx-auto my-24 text-cas" style={{ borderRadius: 40 }}>
-    <div className="container p-24 mx-auto">
+const CTA = ({ label, title, body, cta, transparentBg }) => (
+  <section className={`lg:w-full w-11/12 mx-auto container mx-auto my-24 text-cas ${transparentBg ? 'lg:w-5/6' : 'bg-sunshine'}`} style={{ borderRadius: 40 }}>
+    <div className={`container mx-auto ${transparentBg ? '-my-12' : 'lg:p-24 p-12'}`}>
       <div className="flex flex-col sm:flex-row sm:items-center items-start mx-auto">
         <div className="lg:w-1/2">
-          <h1 className="flex-grow sm:pr-16 text-4xl font-bold title-font leading-tight">{title}</h1>
+          <h1 className="flex-grow sm:pr-16 text-3xl lg:text-4xl font-bold title-font leading-tight">{title}</h1>
           <p className="my-4 text-lg leading-tight">
             <PortableText blocks={body} />
           </p>
         </div>
         <div className="lg:w-1/2 text-right">
-          <CTALink {...cta} kind="button" buttonActionClass="bg-cas text-white text-sm font-medium px-10 py-4 rounded-2xl hover:bg-transparent hover:text-cas border border-cas"></CTALink>
+          <CTALink {...cta} kind="button" buttonActionClass={`bg-cas text-white text-sm font-medium px-10 py-4 rounded-2xl hover:bg-white hover:text-cas font-semibold ${transparentBg ? 'border border-cas' : ''}`}></CTALink>
         </div>
       </div>
     </div>
