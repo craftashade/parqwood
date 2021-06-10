e89dc47844544d5bf2f96f234653c288e73a34c5
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-10 16:59:46 +0800

    Add tailwind colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
         cas: "#20215B",
         sunshine: "#F6C35C",
         grey: "#CFD9E1",
-        airbnb: "#FA565D"
+        airbnb: "#FA565D",
+        f6: "#F6F6F6"
       }
     },
     fontFamily: {
