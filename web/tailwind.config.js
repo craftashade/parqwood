8983bc206252b6f2e52fc91384d67892a7ecd5c7
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-13 14:57:52 +0800

    Add light grey to colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         cas: "#20215B",
         sunshine: "#F6C35C",
         grey: "#CFD9E1",
+        "light-grey": "#EEEEEE",
         airbnb: "#FA565D",
         f6: "#F6F6F6"
       }
