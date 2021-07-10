d967a31c2a1f829f1f73b1f0f5da15102d53811c
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-07-11 00:57:07 +0800

    Add simple 404 page

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,14 +1,119 @@
 import React from "react";
-
-import Layout from "../components/layout";
+import Layout from "../containers/layout";
 import SEO from "../components/seo";
+import { graphql } from "gatsby";
+import Container from "../components/container";
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+export const query = graphql`
+  query NotFoundQuery {
+    page: sanityPage(_id: { regex: "/(drafts.|)frontpage/" }) {
+      ...PageInfo
+    }
+
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+      openGraph {
+        title
+        description
+        image {
+          ...SanityImage
+        }
+      }
+      _rawAddress
+      addressLink
+      mobile
+      tel
+      email
+      banner {
+        color {
+          rgb {
+            r
+            g
+            b
+            a
+          }
+        }
+        message
+        page {
+          ... on SanityRoute {
+            slug {
+              current
+            }
+          }
+        }
+        url
+      }
+    }
+
+    navs: allSanityNavigationMenu {
+      edges {
+        node {
+          title
+          ...NavMenu
+        }
+      }
+    }
+    serviceCategories: allSanityServiceCategory {
+      nodes {
+        title
+        services {
+          title
+        }
+      }
+    }
+    services: allSanityService {
+      nodes {
+        title
+        serviceCategory {
+          title
+        }
+      }
+    }
+    categories: allSanityServiceCategory {
+      nodes {
+        title
+        image {
+          image {
+            ...SanityImage
+          }
+        }
+        _rawImage(resolveReferences: {maxDepth: 10})
+      }
+    }
+    projects: allSanityProject {
+      nodes {
+        images {
+          image {
+            ...SanityImage
+          }
+        }
+        thumbnail {
+          image {
+            ...SanityImage
+          }
+        }
+        title
+        _rawThumbnail(resolveReferences: {maxDepth: 10})
+      }
+    }
+  }
+`;
+
+const NotFoundPage = (props) => {
+  const { data } = props;
+  return (
+    <Layout textWhite={false} data={data}>
+      <Container>
+        <SEO title="404: Not found" />
+        <div className="font-body my-24">
+          <h1 className="text-5xl text-center font-bold text-cas mb-12">Page not found</h1>
+          <div className="w-full text-center">
+            <a href="/" target="_blank" className="text-cas border border-cas rounded-2xl px-12 py-4 font-bold text-sm hover:bg-cas hover:text-white">Back to Home</a>
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  )
+}
 
 export default NotFoundPage;
