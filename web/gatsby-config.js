7f5075df64c6bd5b1ae688046e9cbb4f4de46dee
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-07-13 02:30:56 +0800

    Add GA config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -52,5 +52,20 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-00000000-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: true,
+      },
+    },
   ]
 };
