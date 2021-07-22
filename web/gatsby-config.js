0dbab01a57f6bfe6e1d5a9784108df7adcffc770
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-07-22 16:27:04 +0800

    Add Google tag manager and config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -67,5 +67,30 @@ module.exports = {
         defer: true,
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-NJT8M22",
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { "gtm.start": new Date().getTime(), event: "gtm.js" },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+        // Defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ]
 };
