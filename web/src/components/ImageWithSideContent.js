62876a2d08bc0dc48779d745536e956c78e26d56
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-08-08 22:45:23 +0800

    Update mobile arrangement

--- web/src/components/ImageWithSideContent.js
+++ web/src/components/ImageWithSideContent.js
@@ -9,19 +9,16 @@ const ImageWithSideContent = ({ title, rows }) => {
       <div className="container px-5 pt-40 mx-auto relative">
         <h1 className="text-4xl font-bold mb-10 leading-tight xl:w-1/3 lg:w-1/2 w-full">{title}</h1>
         {rows.map((row, index) => {
-          const img = maybeImage(row.illustration, { borderRadius: 24, height: 400, width: "100%" })
-          const elems = [
-            <>{img && <div>{img}</div>}</>,
-            <div>
-              <h4 className="text-4xl font-bold mb-4 leading-tight">{row.title}</h4>
-              <PortableText blocks={row.body} />
-              <CTALink {...row.cta} buttonActionClass="text-primary border border-primary py-3 px-10 focus:outline-none hover:bg-primary hover:text-white rounded-xl text-sm font-bold mt-4"></CTALink>
-            </div>
-          ]
-          if (index % 2 !== 0) elems.reverse()
+          const img = maybeImage(row.illustration, { borderRadius: 16, height: 400, width: "100%" })
           return (
             <div className="lg:grid grid-cols-2 gap-20 items-center justify-center block w-full mb-16">
-              {elems.map(elem => elem)}
+              {img && <div className={`cut-off-tr ${index % 2 !== 0 ? 'lg:hidden block' : ''}`}>{img}</div>}
+              <div>
+                <h4 className="text-4xl font-bold mb-4 leading-tight mt-6 lg:mt-0">{row.title}</h4>
+                <PortableText blocks={row.body} />
+                <CTALink {...row.cta} buttonActionClass="text-primary border border-primary py-3 px-10 focus:outline-none hover:bg-primary hover:text-white rounded-xl text-sm font-bold mt-4"></CTALink>
+              </div>
+              {index % 2 !== 0 && img && <div className={`cut-off-tr ${index % 2 !== 0 ? 'hidden lg:block' : ''}`}>{img}</div>}
             </div>
           )
         })}
