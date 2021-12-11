3c78a1eb05cc47191c5963e5dc79d3fb64607bba
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-12-11 11:41:29 +0800

    Use new article specific serializer

--- web/src/components/portableText.js
+++ web/src/components/portableText.js
@@ -1,10 +1,12 @@
 import React from "react";
 import clientConfig from "../../client-config";
 import BasePortableText from "@sanity/block-content-to-react";
-import serializers from "./serializers";
+import serializers, { serializersWithoutImage } from "./serializers";
 
-const PortableText = ({ blocks, className }) => (
-  <BasePortableText blocks={blocks} serializers={serializers} {...clientConfig.sanity} />
+const PortableText = ({ blocks, className, isArticle }) => (
+  isArticle ? 
+    <BasePortableText blocks={blocks} serializers={serializersWithoutImage} {...clientConfig.sanity} /> :
+    <BasePortableText blocks={blocks} serializers={serializers} {...clientConfig.sanity} />
 );
 
 export default PortableText;
