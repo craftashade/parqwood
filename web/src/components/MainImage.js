8e33e746f2e708adc3d628dca0cfaa1c4dbe9a76
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-12-11 11:41:05 +0800

    Add new image component that does not resize image

--- web/src/components/MainImage.js
+++ web/src/components/MainImage.js
@@ -15,4 +15,18 @@ const MainImage = ({ mainImage, width = 1200 }) => {
   return imgUrl ? <img src={imgUrl} alt={mainImage.alt || ""} /> : <></>;
 };
 
+export const MainImageRaw = ({ mainImage }) => {
+  const imgUrl =
+    mainImage &&
+    imageUrlFor(buildImageObj(mainImage))
+      .url();
+
+  return imgUrl ? 
+  <div style={{ margin: "1.5rem 0" }}>
+    <img src={imgUrl} alt={mainImage.alt || ""} className="m-auto" /> 
+    <p className="text-center text-sm">{mainImage.caption}</p>
+  </div> : 
+    <></>;
+};
+
 export default MainImage;
